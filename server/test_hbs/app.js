f59b8807f7b6e53e18e5bfd099e167465fe0091e
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2025-02-18 15:40:12 +0200

    some changes

--- server/test_hbs/app.js
+++ server/test_hbs/app.js
@@ -4,14 +4,16 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const expressSession = require('express-session')
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 
-var app = express()
+const app = express()
 
+// express setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
+// express middlewares
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -23,14 +25,13 @@ app.use(expressSession({
 }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// express routers
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     next(createError(404))
 })
-
-app.use(function (err, req, res, next) {
+app.use( (err, req, res, next) => {
     res.locals.message = err.message
     res.locals.error = req.app.get('env') === 'development' ? err : {}
 
